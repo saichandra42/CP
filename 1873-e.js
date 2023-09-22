0b6901de75d6a480836c49bb21e34acb297c1420
Sai Chandra <saichandra004@hotmail.com>
2023-09-22 00:02:12 -0700

    Codeforces

---
+++ 1873-e.js
@@ -0,0 +1,19 @@
+let i = '';
+process.stdin.on('data', c => i += c);
+process.stdin.on('end', () => {
+    const {EOL} = require('os');
+    const input = i.split(EOL);
+    const test_cases = Number(input[0]);
+    let line = 1;
+    for (let i = 0; i < test_cases; i++) {
+        const [n, x] = input[line++].trim().split(' ').map(Number);
+        const a = input[line++].trim().split(' ').map(Number);
+        const res = solution(n, x, a);
+        console.log(res);
+    }
+});
+
+
+function solution(n, x, a) {
+    
+}
